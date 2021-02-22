2eef3529d25d87742494b7cea42f392c63025c6c
gurdipsingh <gurdipsingh1990@gmail.com>
2021-02-21 20:55:15 -0800

    finsihed a subset of algros

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,47 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    // assinging strA the longest length string, makes error easier to catch when comparing
+    // two mapped string objects 
+    const strA = stringA.length > stringB.length ? stringA : stringB; 
+    const strB = stringB.length < stringA.length ? stringB : stringA; 
+
+    const mappedStringA = mapString(strA.toLowerCase());
+    const mappedStringB = mapString(strB.toLowerCase());
+    let isAnagram = false; 
+    
+    for (let prop in mappedStringA) {
+        isAnagram = mappedStringA[prop] === mappedStringB[prop] ? true : false; 
+    }
+
+    return isAnagram;
+}
+
+const mapString = (str) => {
+    const strMap = {};
+    const strSplitIntoArr = str.split('');
+
+    for (let char of strSplitIntoArr) {
+        // does not allow special characters 
+        if (char.match(/[a-z]/i)) {
+            if (strMap[char] === undefined) {
+                strMap[char] = 1;
+            } else {
+                strMap[char]++;
+            }
+        }
+    }
+    return strMap;
+}
+
+anagrams('RAIL! SAFETY! c', 'fairy tales')
 
 module.exports = anagrams;
+
+
+
+/*
+I've mapped out two strings, lowercased letters since it does not matter, and used regex to only let in characters.
+
+*/
